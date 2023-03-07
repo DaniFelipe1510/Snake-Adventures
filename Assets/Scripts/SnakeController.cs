ebb28a06c0996b3d54bfaf5b1cb7d02e1e967e3b
JulianSP02 <jsolis30315@ufide.ac.cr>
2023-03-07 14:55:42 -0600

    Movimiento Snake

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -4,7 +4,45 @@ using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
+    [SerializeField]
+    float speed = 10F;
+
+
+    Vector2 direction = Vector2.zero;
     float points;
+
+    void Start()
+    {
+        InvokeRepeating("UpdatePosition", 0, 1 / speed);
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if(inputX > 0)
+        {
+            direction = Vector2.right;
+        }else if(inputX < 0)
+        {
+            direction = Vector2.left;
+        }else if(inputY > 0)
+        {
+            direction = Vector2.up;
+        }else if (inputY < 0)
+        {
+            direction = Vector2.down;
+        }
+    }
+
+    void UpdatePosition()
+    {
+        Vector3 headPosition = this.transform.position;
+        Vector3 newPosition = new Vector3(headPosition.x + direction.x, headPosition.y + direction.y, 0.0F);
+        transform.position = newPosition;
+    }
+
     public void IncreasePoints(float value)
     {
         points += value;
