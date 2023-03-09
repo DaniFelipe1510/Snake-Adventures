6465817b0c8c290e8b657ddfdadec66a8475dc05
JulianSP02 <jsolis30315@ufide.ac.cr>
2023-03-08 20:04:07 -0600

    Mejora del movimiento de Snake
    En este caso, el movimiento es como el de una serpiente literalmente, puede moverse diagonalmente, se detiene, etc.

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -7,40 +7,14 @@ public class SnakeController : MonoBehaviour
     [SerializeField]
     float speed = 10F;
 
+    Vector3 direction = Vector3.zero;
 
-    Vector2 direction = Vector2.zero;
     float points;
 
-    void Start()
-    {
-        InvokeRepeating("UpdatePosition", 0, 1 / speed);
-    }
 
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-
-        if(inputX > 0)
-        {
-            direction = Vector2.right;
-        }else if(inputX < 0)
-        {
-            direction = Vector2.left;
-        }else if(inputY > 0)
-        {
-            direction = Vector2.up;
-        }else if (inputY < 0)
-        {
-            direction = Vector2.down;
-        }
-    }
-
-    void UpdatePosition()
-    {
-        Vector3 headPosition = this.transform.position;
-        Vector3 newPosition = new Vector3(headPosition.x + direction.x, headPosition.y + direction.y, 0.0F);
-        transform.position = newPosition;
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * Time.deltaTime * speed;
     }
 
     public void IncreasePoints(float value)
